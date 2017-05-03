c551bf5c388ec19ed015ce4eae795559f7112889
Kazuki Sakamoto <sakamoto@splhack.org>
2017-05-03 08:08:05 -0700

    Use MacVim.app/Contents/bin/mvim script by Xcode build

--- macvim.rb
+++ macvim.rb
@@ -64,27 +64,15 @@ class Macvim < Formula
     prefix.install apppath
 
     appbin = prefix + "MacVim.app/Contents/bin"
-    mkdir_p appbin
-
     bin = prefix + 'bin'
     mkdir_p bin
 
-    mvim = appbin + "mvim"
-    cp 'src/MacVim/mvim.sh', mvim
-    chmod 0755, mvim
-
     [
-      {:path => appbin, :src => 'mvim'},
-      {:path => bin, :src => '../MacVim.app/Contents/bin/mvim'},
-    ].each do |link|
-      [
-        'vim', 'vimdiff', 'view',
-        'gvim', 'gvimdiff', 'gview',
-        'mvim', 'mvimdiff', 'mview'
-      ].each do |t|
-        dst = link[:path] + t
-        ln_s link[:src], dst unless File.exists? dst
-      end
+      'vim', 'vimdiff', 'view',
+      'gvim', 'gvimdiff', 'gview',
+      'mvim', 'mvimdiff', 'mview'
+    ].each do |t|
+      ln_s '../MacVim.app/Contents/bin/mvim', bin + t
     end
   end
 
