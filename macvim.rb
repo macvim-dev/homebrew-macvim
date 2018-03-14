d512e99df73cfb48033f6531877beb6f16818a4f
Kazuki Sakamoto <sakamoto@splhack.org>
2018-03-13 21:28:30 -0700

    Revert "Sync Homebrew python change"
    This reverts commit 6b3e765328a72d00a73e5c5dd65606a1f4447e2d.

--- macvim.rb
+++ macvim.rb
@@ -9,7 +9,7 @@ class Macvim < Formula
 
   depends_on 'gettext' => :build
   depends_on 'lua' => :build
-  depends_on 'python' => :build
+  depends_on 'python3' => :build
 
   def get_path(name)
     f = Formulary.factory(name)
@@ -24,7 +24,6 @@ class Macvim < Formula
     perl_version = '5.16'
     ENV.append 'VERSIONER_PERL_VERSION', perl_version
     ENV.append 'VERSIONER_PYTHON_VERSION', '2.7'
-    ENV.append 'vi_cv_path_python', '/usr/bin/python'
     ENV.append 'vi_cv_path_python3', "#{HOMEBREW_PREFIX}/bin/python3"
     ENV.append 'vi_cv_path_plain_lua', "#{HOMEBREW_PREFIX}/bin/lua"
     ENV.append 'vi_cv_dll_name_perl', "/System/Library/Perl/#{perl_version}/darwin-thread-multi-2level/CORE/libperl.dylib"
